5e943ad0f99cf1e6fbf4f9bdcff216d451c1b036
Ilya Dorman <ilya@traxretail.com>
2016-11-01 18:36:56 +0200

    added basic list functionality

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 angular.module('input-with-tags', [])
 
 
+
 .controller('ctrl', function($scope) {
-    $scope.addTag = 'sdfdsf';
-    $scope.tags1 = ['dsfds','dsdffd'];
+    $scope.tags1 = [
+        'dsfds',
+        'dsdffd'
+    ];
+    $scope.options1 = [
+        'ilya',
+        'slavo',
+        'eyal',
+        'gil',
+        'nir'
+    ];
 })
+
 
 
 .directive('textTags', function ($timeout) {
@@ -12,17 +23,21 @@ angular.module('input-with-tags', [])
     function tagsAddVisualPadding(input, tagList) {
         $timeout(function () {
             var tagListLength = Math.floor(tagList.getBoundingClientRect().width);
-            input.css({
-                'padding-left':
-                tagListLength + 5 + 'px'
-            });
+            input.css({ 'padding-left' : tagListLength + 10 + 'px' });
         });
+    }
+
+    function removeSelectedTags (selectedTags, allTags) {
+        return _.pullAll(allTags, selectedTags);
     }
 
     return {
         restrict: 'E',
         templateUrl: 'text-tags.html',
-        scope: { tags: '=tags' },
+        scope: {
+            tags: '=tags',
+            options: '=options'
+        },
         link: function(scope, element, attr) {
             var input = element.find('input');
             var tagList = element.find('ul');
@@ -30,19 +45,27 @@ angular.module('input-with-tags', [])
             tagsAddVisualPadding(input, tagList[0]);
 
             scope.tagText = '';
+            scope.options = removeSelectedTags(scope.tags, scope.options);
+            scope.bla = function (e) { console.log(e) };
+
             scope.addTag = function (e) {
 
                 if (e.key === 'Enter') {
                     scope.tags.push(scope.tagText);
                     scope.tagText = '';
+                    scope.options = removeSelectedTags(scope.tags, scope.options);
 
                     // for some ridiculous reason focus() only works with timeout.
-                    $timeout(function () { input[0].focus(); });
+                    $timeout(function () {
+                        input[0].focus();
+                    });
+
                     tagsAddVisualPadding(input, tagList[0]);
 
                 } else if (e.key === 'Backspace') {
                     if (scope.tagText === '') {
-                        scope.tags.pop();
+                        var deletedTag = scope.tags.pop();
+                        scope.options.push(deletedTag);
                         tagsAddVisualPadding(input, tagList[0]);
                     }
                 }
